b515f5c9c10397842dcb84b08bffa6f25d9dd7e1
Phil Henault <philhen27@gmail.com>
2019-01-21 15:36:15 -0500

    test

--- hello_world_2.py
+++ hello_world_2.py
@@ -47,3 +47,39 @@ value2 = hello_world("HELLO WORLD", 2)
 #Test
 
 
+
+def hello_world(string, value):
+    """
+    Returns True if a value is greater than 1, returns False otherwise.
+    """
+    if value > 1:
+        print(string)
+        return True 
+    else:
+        return False
+
+
+value = hello_world("HELLO WORLD", 2)
+
+
+
+value1 = hello_world("HELLO WORLD", 2)
+
+value2 = hello_world("HELLO WORLD", 2)
+
+#Test
+#Test
+#Test
+
+for i in range(0,10):
+	if i % 2 == 0:
+		print(i)
+
+value1 = hello_world("HELLO WORLD", 2)
+
+value2 = hello_world("HELLO WORLD", 2)
+
+#Test
+#Test
+#Test
+
