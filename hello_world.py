3240f1947a30fb11aa47949a4fc5d2d9366d8448
Phil Henault <philhen27@gmail.com>
2019-01-21 23:33:44 -0500

    test

--- hello_world.py
+++ hello_world.py
@@ -11,27 +11,6 @@ Contains a function that returns True if a value
 is greater than 1, returns False otherwise.
 """
 
-def hello_world(string, value):
-    """
-    Returns True if a value is greater than 1, returns False otherwise.
-    """
-    if value > 5:
-        print(string)
-        return True 
-    else:
-        return False
-
-
-def hello_world2(string, value):
-    """
-    Returns True if a value is greater than 1, returns False otherwise.
-    """
-    if value > 1:
-        print(string)
-        return True 
-    else:
-        return False
-
 
 def hello_world2(string, value):
     """
