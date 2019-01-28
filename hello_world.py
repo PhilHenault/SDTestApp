0250bb98d1c84199ae4efda2143e76ff7af590cc
Phil Henault <philhen27@gmail.com>
2019-01-27 21:42:25 -0500

    test

--- hello_world.py
+++ hello_world.py
@@ -32,6 +32,27 @@ def hello_world2(string, value):
     else:
         return False
 
+def hello_world2(string, value):
+    """
+    Returns True if a value is greater than 1, returns False otherwise.
+    """
+    if value > 1:
+        print(string)
+        return True 
+    else:
+        return False
+
+def hello_world2(string, value):
+    """
+    Returns True if a value is greater than 1, returns False otherwise.
+    """
+    if value > 1:
+        print(string)
+        return True 
+    else:
+        return False
+
+
 
 
 
