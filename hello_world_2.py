5c6ce92e6d1b8cd35d2b78d83b8db2e83e1049f8
Phil Henault <philhen27@gmail.com>
2019-04-20 14:57:29 -0400

    testing

--- hello_world_2.py
+++ hello_world_2.py
@@ -81,59 +81,5 @@ def hello_world(string, value):
         return False
 
 
-def hello_world(string, value):
-    """
-    Returns True if a value is greater than 1, returns False otherwise.
-    """
-    if value > 1:
-        print(string)
-        return True 
-    else:
-        return False
-
-
-def hello_world(string, value):
-    """
-    Returns True if a value is greater than 1, returns False otherwise.
-    """
-    if value > 1:
-        print(string)
-        return True 
-    else:
-        return False
-
-
-def hello_world(string, value):
-    """
-    Returns True if a value is greater than 1, returns False otherwise.
-    """
-    if value > 1:
-        print(string)
-        return True 
-    else:
-        return False
-
-
-
-def hello_world(string, value):
-    """
-    Returns True if a value is greater than 1, returns False otherwise.
-    """
-    if value > 1:
-        print(string)
-        return True 
-    else:
-        return False
-
-
-def hello_world(string, value):
-    """
-    Returns True if a value is greater than 1, returns False otherwise.
-    """
-    if value > 1:
-        print(string)
-        return True 
-    else:
-        return False
 
 
