fd42422af186320ea6baa287424863a7838fcbad
Phil Henault <philhen27@gmail.com>
2019-04-22 18:28:21 -0400

    added test_file.py

--- test_file.py
+++ test_file.py
@@ -11,4 +11,18 @@ time_now = localtime()
 hour = time_now.tm_hour
 
 
+activities = {8: 'Sleeping',
+              9: 'Commuting',
+              17: 'Working',
+              18: 'Commuting',
+              20: 'Eating',
+              22: 'Resting' }
+
+time_now = localtime()
+hour = time_now.tm_hour
+
+
+
+
+
 
