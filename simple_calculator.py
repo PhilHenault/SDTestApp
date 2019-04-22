42db393e0d5fc882099843413ea9c0c364539000
Phil Henault <philhen27@gmail.com>
2019-04-22 18:44:35 -0400

    testing

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,3 +1,26 @@
+"""
+Written by  : Shreyas Daniel - github.com/shreydan
+Description : Uses Pythons eval() function
+              as a way to implement calculator.
+             
+Functions available are:
+--------------------------------------------
+                         + : addition
+                         - : subtraction
+                         * : multiplication
+                         / : division
+                         % : percentage
+                         e : 2.718281...
+                        pi : 3.141592... 
+                      sine : sin(rad)
+                    cosine : cos(rad)
+                   exponent: x^y
+                   tangent : tan(rad)
+                 remainder : XmodY
+               square root : sqrt(n)
+  round to nearest integer : round(n)
+convert degrees to radians : rad(deg)
+"""
 
 import sys
 
@@ -5,7 +28,13 @@ from fileinfo import raw_input
 
 
 def calc(term):
+    """
+        input: term of type str
+        output: returns the result of the computed term.
+        purpose: This function is the actual calculator and the heart of the application
+    """
 
+    # This part is for reading and converting arithmetic terms.
     term = term.replace(' ', '')
     term = term.replace('^', '**')
     term = term.replace('=', '')
@@ -16,6 +45,7 @@ def calc(term):
 
     functions = ['sin', 'cos', 'tan', 'pow', 'cosh', 'sinh', 'tanh', 'sqrt', 'pi', 'radians', 'e']
 
+    # This part is for reading and converting function expressions.
     term = term.lower()
 
     for func in functions:
@@ -25,8 +55,10 @@ def calc(term):
 
     try:
 
+        # here goes the actual evaluating.
         term = eval(term)
 
+    # here goes to the error cases.
     except ZeroDivisionError:
 
         print("Can't divide by 0.  Please try again.")
@@ -45,11 +77,21 @@ def calc(term):
 
 
 def result(term):
-
+    """
+        input:  term of type str
+        output: none
+        purpose: passes the argument to the function calc(...) and 
+                prints the result onto console.
+    """
     print("\n" + str(calc(term)))
 
 
 def main():
+    """
+        main-program
+        purpose: handles user input and prints 
+                 information to the console.
+    """
 
     print("\nScientific Calculator\n\nFor Example: sin(rad(90)) + 50% * (sqrt(16)) + round(1.42^2)" +
           "- 12mod3\n\nEnter quit to exit")
